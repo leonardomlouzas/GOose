815ed6f84d7a996b180592175e7dabc56b4bfe88
Leonardo Moreira Louzas <leonardomlouzas@gmail.com>
2025-06-19 17:38:07 -0300

    CH1L11 Done

--- main.go
+++ main.go
@@ -10,15 +10,25 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+
+	
+	
 	server := &http.Server {
 		Addr:		":" + port,
 		Handler: 	mux,
 	}
-	
-	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Printf("Serving files from %s under /app/ on port: %s\n", filepathRoot, port)
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		log.Fatalf("Could not listen on %s: %v\n", server.Addr, err)
 	}
+	
+}
 
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
