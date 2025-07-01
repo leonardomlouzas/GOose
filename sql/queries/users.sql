ff160f00f5725d1587d5e7cedabcd263e4463a19
Leonardo Moreira Louzas <leonardomlouzas@gmail.com>
2025-07-01 16:07:28 -0300

    CH8L1

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -9,6 +9,12 @@ SET email = $2, hashed_password = $3, updated_at = $4
 WHERE id = $1
 RETURNING *;
 
+-- name: AlterUserPremiumStatus :one
+UPDATE users
+SET is_chirpy_red = $2, updated_at = $3
+WHERE id = $1
+RETURNING *;
+
 -- name: GetUserById :one
 SELECT * FROM users WHERE id = $1;
 
